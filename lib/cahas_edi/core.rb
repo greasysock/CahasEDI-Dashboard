3be6cda4cc0e7e8f75f542decf15360b6565deed
chris <chris.gresock@gmail.com>
2019-03-04 14:56:48 -0500

    started work on CahasEDI library for communicating to backend

---
+++ lib/cahas_edi/core.rb
@@ -0,0 +1,15 @@
+module CahasEdi
+    class core
+        def inititialize
+        end
+
+        def get_partners
+        end
+
+        def get_messages
+        end
+
+        def get_templates
+        end
+    end
+end
