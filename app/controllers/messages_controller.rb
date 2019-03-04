9c933ca9a31e610affa663b6c657e220d2aace48
chris <chris.gresock@gmail.com>
2019-03-04 18:59:03 -0500

    Added messages controller with new views

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,22 @@
+class MessagesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_message, only: [:show]
+    def index
+        @messages = CahasEdi::Core.messages
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    private
+
+    def set_message
+        @message = CahasEdi::Core.message params[:id]
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'page#home'
   get 'invoices', to: 'page#invoices'
-  get 'messages', to: 'page#messages'
+
+  resources :messages, except: [:destroy, :edit, :update]
+  #get 'messages', to: 'page#messages'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
